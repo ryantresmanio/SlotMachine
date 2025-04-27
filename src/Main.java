f7b9490b637477355b97e8ed44298f52fda5cd3b
unodostres123456 <rayantres94@gmail.com>
2025-04-28 00:23:51 +0800

    fixed some words and emojis

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -10,16 +9,15 @@ public class Main {
         //DECLARE VARIABLES
         int balance = 50;
         int bet;
-        int payout;
         String[] row;
 
         //GREET
         System.out.println("\n---------------------------");
-        System.out.println("Welcome to My Slut Machine!");
-        System.out.println("Symbols: 🍌 🥒 🍆 💦 🌭 🍑");
+        System.out.println("Welcome to My Slot Machine!");
+        System.out.println("Symbols: ⭐ ♦️ 🤑 🫰 💝 💫");
         System.out.println("---------------------------");
 
-        //PLAY IF BALANCE  . 0
+        //PLAY IF BALANCE > 0
         while (balance > 0) {
             System.out.println("\nCurrent Balance: $" + balance);
             //ENTER AMOUNT BET
@@ -48,11 +46,9 @@ public class Main {
             printRow(row);
 
             if (row[0].equals(row[1]) && (row[1].equals(row[2]))){
-                System.out.println("🎊🎊 YOU WIN $" + bet*3);
+                System.out.println("🎊🎊 YOU WON $" + bet*3);
                 balance = balance + (bet*3);
             }
-            //System.out.println(Arrays.toString(row));
-            //row = spinRow();
 
             System.out.print("Do you want to play again? (Y/N) ");
             String playAgain = scan.nextLine().toUpperCase();
@@ -73,13 +69,13 @@ public class Main {
             System.out.println("O balance left");
         }
 
-        System.out.println("Thank You for playing my silly game");
+        System.out.println("Thank You for playing my game");
         System.out.println("Your Balance; " + balance);
         scan.close();
     }
 
     static String[] spinRow() {
-        String[] symbols = {"🍌", "🥒", "🍆", "💦", "🌭", "🍑"};
+        String[] symbols = {"⭐", "♦️", "🤑", "🫰", "💝", "💫"};
         String[] row = new String[3];
         Random random = new Random();
 
@@ -89,7 +85,6 @@ public class Main {
             row[i] = symbols[random.nextInt(symbols.length)];
         }
 
-        //System.out.println(row[0] + row[1] + row[2]);
         return row;
     }
 
